0bbf4c40246a580c92e4bb0651850678a5ef274d
P2SAMAPA <pranabpattanaik@gmail.com>
2026-05-13 18:08:46 +0800

    Create pid_controller.py

---
+++ pid_controller.py
@@ -0,0 +1,16 @@
+class PIDController:
+    def __init__(self, kp, ki, kd, setpoint=0.0):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.setpoint = setpoint
+        self.integral = 0.0
+        self.prev_error = 0.0
+
+    def update(self, measurement, dt=1.0):
+        error = self.setpoint - measurement
+        self.integral += error * dt
+        derivative = (error - self.prev_error) / dt if dt > 0 else 0.0
+        output = self.kp * error + self.ki * self.integral + self.kd * derivative
+        self.prev_error = error
+        return output
